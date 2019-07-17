0a47e95b6dd83a19a7b91b0610ab475371a5f1b8
Vasily93 <markovavasily@gmail.com>
2019-07-17 08:35:55 -0700

    route and test to get all memes

---
+++ test/memes.test.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+const app = require('../lib/app');
+const request = require('supertest');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Meme = require('../lib/moduls/Meme');
+
+describe('testing all meme routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.close();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+  
+  it('creates a meme with PUT', () => {
+    return request(app)
+      .post('/api/v1/memes')
+      .send({
+        topField: 'some other text',
+        image: 'some url',
+        bottomField: 'some text'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          topField: 'some other text',
+          image: 'some url',
+          bottomField: 'some text',
+          __v:0
+        });  
+      });
+  });
+
+  it('gets list of all memes', async() => {
+    const meme = await Meme.create({ 
+      topField: 'some other text',
+      image: 'some url',
+      bottomField: 'some text' 
+    });
+
+    return request(app)
+      .get('api/v1/memes')
+      .then(res => {
+        expect(res.body).toEqual([meme]);
+      });
+  })
+});
