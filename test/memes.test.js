7a67918e9293ddedfea4f9865e899caf5fde0805
Vasily93 <markovavasily@gmail.com>
2019-07-17 08:54:14 -0700

    test for get by id route

--- test/memes.test.js
+++ test/memes.test.js
@@ -48,7 +48,28 @@ describe('testing all meme routes', () => {
     return request(app)
       .get('api/v1/memes')
       .then(res => {
-        expect(res.body).toEqual([meme]);
+        const memeJSON = JSON.parse(JSON.stringify(meme));
+        expect(res.body).toEqual([memeJSON]);
       });
-  })
+  });
+
+  it('gets meme by id', async() => {
+    const meme = await Meme.create({
+      topField: 'some other text',
+      image: 'some url',
+      bottomField: 'some text' 
+    });
+
+    return request(app)
+      .get(`/api/v1/memes/${meme._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id:expect.any(String),
+          topField: 'some other text',
+          image: 'some url',
+          bottomField: 'some text',
+          __v: 0 
+        });
+      });
+  });
 });
