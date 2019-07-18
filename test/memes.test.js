5a6dfc505128efdae6042269a4cefbb7cf10c9c0
Vasily93 <markovavasily@gmail.com>
2019-07-18 04:26:56 -0700

    test and route to get meme by id

--- test/memes.test.js
+++ test/memes.test.js
@@ -4,16 +4,16 @@ const app = require('../lib/app');
 const request = require('supertest');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
-// const Meme = require('../lib/moduls/Meme');
+const Meme = require('../lib/moduls/Meme');
 
 describe('testing all meme routes', () => {
   beforeAll(() => {
     connect();
   });
 
-  beforeEach(() => {
-    return mongoose.connection.close();
-  });
+  // beforeEach(() => {
+  //   return mongoose.connection.close();
+  // });
 
   afterAll(() => {
     return mongoose.connection.close();
@@ -38,38 +38,38 @@ describe('testing all meme routes', () => {
       });
   });
 
-  // it('gets list of all memes', async() => {
-  //   const meme = await Meme.create({ 
-  //     topField: 'some other text',
-  //     image: 'some url',
-  //     bottomField: 'some text' 
-  //   });
+  it('gets list of all memes', async() => {
+    const meme = await Meme.create({ 
+      topField: 'some other text',
+      image: 'some url',
+      bottomField: 'some text' 
+    });
 
-  //   return request(app)
-  //     .get('api/v1/memes')
-  //     .then(res => {
-  //       const memeJSON = JSON.parse(JSON.stringify(meme));
-  //       expect(res.body).toEqual([memeJSON]);
-  //     });
-  // });
+    return request(app)
+      .get('/api/v1/memes')
+      .then(res => {
+        const memeJSON = JSON.parse(JSON.stringify(meme));
+        expect(res.body).toEqual([memeJSON]);
+      });
+  });
 
-  // it('gets meme by id', async() => {
-  //   const meme = await Meme.create({
-  //     topField: 'some other text',
-  //     image: 'some url',
-  //     bottomField: 'some text' 
-  //   });
+  it('gets meme by id', async() => {
+    const meme = await Meme.create({
+      topField: 'some other text',
+      image: 'some url',
+      bottomField: 'some text' 
+    });
 
-  //   return request(app)
-  //     .get(`/api/v1/memes/${meme._id}`)
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id:expect.any(String),
-  //         topField: 'some other text',
-  //         image: 'some url',
-  //         bottomField: 'some text',
-  //         __v: 0 
-  //       });
-  //     });
-  // });
+    return request(app)
+      .get(`/api/v1/memes/${meme._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          topField: 'some other text',
+          image: 'some url',
+          bottomField: 'some text',
+          __v: 0 
+        });
+      });
+  });
 });
